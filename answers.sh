f92e8396611a6c66a0ba8e6fee17f8723e6a13e6
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-06-05 01:20:14 +0300

    basic queries Done

---
+++ answers.sh
@@ -0,0 +1,14 @@
+Q1:Basic Queries
+1- select name from students;
+2-select name,age from students where age >30;
+3-select * from students where age =30 and age ="F";
+4-select points from students where name ="Alex";
+5-insert into students (
+id,name,age,gender,points)
+values(
+8,
+"bob",24,"M",350
+);
+6-update students set points=points+250 where name="basma"; 
+7-update students set points=points-100 where name="Alex"; 
+

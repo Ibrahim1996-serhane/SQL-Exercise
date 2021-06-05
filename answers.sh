ec04f2e4fd80a0c69ef82e9db5360114a54202f3
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-06-05 14:26:43 +0300

    create -table

--- answers.sh
+++ answers.sh
@@ -11,4 +11,21 @@ values(
 );
 6-update students set points=points+250 where name="basma"; 
 7-update students set points=points-100 where name="Alex"; 
-
+Q2:Creating Table
+----------------
+CREATE TABLE "graduates" (
+	"ID"	INTEGER NOT NULL,
+	"Name"	TEXT NOT NULL UNIQUE,
+	"Age"	INTEGER,
+	"Gender"	TEXT,
+	"Points"	INTEGER,
+	"Graduation"	TEXT,
+	PRIMARY KEY("ID")
+);
+1-INSERT INTO graduates (ID,Name,Age,Gender,Points) 
+  SELECT ID,Name , Age , Gender ,Points FROM students 
+  WHERE Name = "Layal";
+2-UPDATE graduates
+ SET Graduation="08/09/2018"
+ WHERE name = "Layal";
+3-DELETE FROM students WHERE name = "Layal";
